c83620472d929c1107789544ba28d965e1f60dd4
Vyachaslsav75 <kslava@tut.by>
2024-02-19 20:06:52 +0300

    Add tests

--- Test/seminars/second/hw/VehicleTest.java
+++ Test/seminars/second/hw/VehicleTest.java
@@ -1,9 +1,51 @@
 package seminars.second.hw;
 
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class VehicleTest {
+    @Test
+    public void testWheelsCar(){
+        Car car = new Car("Ford", "Escort", 2020);
+        assertThat(car.getNumWheels()).isEqualTo(4);
+    }
+
+    @Test
+    public void testWheelsMoto(){
+        Motorcycle motorcycle = new Motorcycle("Yamaha", "Bandit", 2022);
+        assertThat(motorcycle.getNumWheels()).isEqualTo(2);
+    }
+
+    @Test
+    public void testSpeedCar(){
+        Car car = new Car("Ford", "Escort", 2020);
+        car.testDrive();
+        assertThat(car.getSpeed()).isEqualTo(60);
+    }
+
+    @Test
+    public void testSpeedMoto(){
+        Motorcycle motorcycle = new Motorcycle("Yamaha", "Bandit", 2022);
+        motorcycle.testDrive();
+        assertThat(motorcycle.getSpeed()).isEqualTo(75);
+    }
+
+    @Test
+    public void testParkCar(){
+        Car car = new Car("Ford", "Escort", 2020);
+        car.testDrive();
+        car.park();
+        assertThat(car.getSpeed()).isEqualTo(0);
+    }
+
+    @Test
+    public void testParkMoto(){
+        Motorcycle motorcycle = new Motorcycle("Yamaha", "Bandit", 2022);
+        motorcycle.testDrive();
+        motorcycle.park();
+        assertThat(motorcycle.getSpeed()).isEqualTo(0);
+    }
 
 }
